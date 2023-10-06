6f8f9e0ceb5a802b385364726003a15405fb5ec3
Gift Aterezi <giftaterezi@gmail.com>
2023-10-06 14:55:43 -0700

    alphabet ecception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        if (letter != 'e' && letter != 'q')
+            putchar(letter);
+
+        letter++;
+    }
+
+    putchar('\n');
+    return 0;
+}
+
