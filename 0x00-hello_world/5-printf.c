c0a295a319721d078a048f195ebb15fa87b5e958
Gift Aterezi <giftaterezi@gmail.com>
2023-10-05 20:11:47 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -this prints the string exactly
+ * programming is like building
+ * followed by a new line
+ * Return: always 0 (succes)
+*/
+int main(void)
+{
+	/*this line prints out a string */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+
+
+}
