b45fea9a3bf6209cd86a67aafbb2b7d758df6b6f
Gift Aterezi <giftaterezi@gmail.com>
2023-10-06 15:28:57 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ * Description: this code prints out number from 0 to 10
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a = 0;
+	
+	while (a <=10)
+	{
+		putchar(a);
+		a++;
+	}
+
+	Putchar("\n");
+	return (0);
+}
