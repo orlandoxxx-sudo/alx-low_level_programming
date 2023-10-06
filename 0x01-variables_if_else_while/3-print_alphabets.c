0930b2aabf736133c03190f653fd1fcdfc4c7564
Gift Aterezi <giftaterezi@gmail.com>
2023-10-06 14:10:50 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: A code that prints all lowercase letters
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int asciiValue = 97;
+
+	while (asciiValue <= 122)
+	{
+		putchar(asciiValue);
+		asciiValue++;
+	}
+
+	asciiValue = 65;
+	while (asciiValue <= 90)
+	{
+		putchar(asciiValue);
+		asciiValue++;
+	}
+	putchar('\n');
+	return (0);
+}
+
