71401b554534e81b8df483b27aeee74f84fa2b2e
Gift Aterezi <giftaterezi@gmail.com>
2023-10-06 11:33:27 -0700

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program checks for last digit and compare
+ * Return: Alwauys 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int lastdigit = n % 10;
+
+	if (last
+	return (0);
+}
