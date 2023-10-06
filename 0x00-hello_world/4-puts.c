d28d8bda29cf78a2b6960556cc123d3a04342682
Gift Aterezi <giftaterezi@gmail.com>
2023-10-05 19:56:56 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
+	/*this line prints out a string */
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 
 
 
