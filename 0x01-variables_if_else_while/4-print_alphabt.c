6ca759b49cb18ab5daf078637fbb3a0708828141
Gift Aterezi <giftaterezi@gmail.com>
2023-10-06 15:18:04 -0700

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Descrioption: a code that prints out all alphabet with a few extensions
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        if (letter != 'e' && letter != 'q')
-            putchar(letter);
-
-        letter++;
-    }
-
-    putchar('\n');
-    return 0;
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return 0;
 }
-
