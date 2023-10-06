04bba1d89ec7049b9880a59fba4883f3721c2577
Gift Aterezi <giftaterezi@gmail.com>
2023-10-06 15:10:15 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,22 +7,12 @@
 */
 int main(void)
 {
-/*	int asciiValue = 97;
+	int asciiValue = 97;
 
 	while (asciiValue <= 122)
 	{
 		putchar(asciiValue);
 		asciiValue++;
-	}
-	putchar('\n');
-	return (0);*/
-
-	char letter= 'a';
-
-	while (letter <= 'z')
-	{
-		putchar(letter);
-		letter++;
 	}
 	putchar('\n');
 	return (0);
