915b59c872e6384cd233b267e53d352e70d13964
Gift Aterezi <giftaterezi@gmail.com>
2023-10-06 21:36:05 -0700

    alpha numberical

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - This program prints all possible combinations of two two-digit numbers
+ * main - This program prints all combinations of two two-digit numbers
  * Return: Always 0 (Success)
 */
 int main(void)
