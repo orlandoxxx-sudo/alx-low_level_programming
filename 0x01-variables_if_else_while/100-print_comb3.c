813f12b26b5dd06938dee1ab57939f25c74e986f
Gift Aterezi <giftaterezi@gmail.com>
2023-10-06 21:57:28 -0700

    numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i, j;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
@@ -23,7 +23,7 @@ int main(void)
 			}
 		}
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
