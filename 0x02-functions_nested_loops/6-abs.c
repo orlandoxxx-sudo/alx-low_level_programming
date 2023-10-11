a3370c0378ad7e06b53a321999028a4ce92785a6
Gift Aterezi <giftaterezi@gmail.com>
2023-10-11 14:37:53 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - this programming returns the absolute valkues of integer
+ * Description: ouputing ab s values of all integers
+ * Return: 0 (Sucess)
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
