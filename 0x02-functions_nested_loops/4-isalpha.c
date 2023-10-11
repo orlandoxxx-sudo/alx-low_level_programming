44372d46f7c2ae0fc1f95ab21c0061c403e46437
Gift Aterezi <giftaterezi@gmail.com>
2023-10-10 22:38:43 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: the Character check
  * Return: 1 if c is lower case, otherwise Return: 0
 */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
