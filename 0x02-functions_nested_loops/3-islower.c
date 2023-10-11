f0765ea514fb55e35656f65709c20a04c2694adb
Gift Aterezi <giftaterezi@gmail.com>
2023-10-10 22:11:40 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 
 /**
  * _islower - checks for lowercase characters and return 1 or 0 otherwise
+ * @c: the Character check
+ * Return: 1 if c is lower case, otherwise Return: 0
 */
 int _islower(int c)
 {
