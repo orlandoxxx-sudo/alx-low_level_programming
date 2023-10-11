2966a4cb0e95f640c250ce144c9a322406f3bf10
Gift Aterezi <giftaterezi@gmail.com>
2023-10-11 15:03:49 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,13 @@
 
 /**
  * print_last_digit - this functions prints the last digit of any number
- * @n: is use to hold the last digit 
+ * @n: is use to hold the last digit
  * Return: Always 0 (success)
 */
 int print_last_digit(int n)
 {
 	int lastdigit;
+
 	lastdigit = n % 10;
 	if (lastdigit < 0)
 		lastdigit = -lastdigit;
