d1c0fe5f608317aeb929686cfc1c0edeaf5c5313
Gift Aterezi <giftaterezi@gmail.com>
2023-10-10 21:46:37 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
+/**
+ * Description - prints alphabet from a to z
+*/
 void print_alphabet(void)
 {
 	int i = 'a';
+
 	while (i <= 'z')
 	{
 		_putchar(i);
 		i++;
 	}
 	_putchar('\n');
-	
+
 }
