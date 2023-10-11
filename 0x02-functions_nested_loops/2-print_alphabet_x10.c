8967fed5c2deb4147b2cf08ff7a4d61c48eee035
Gift Aterezi <giftaterezi@gmail.com>
2023-10-10 22:00:44 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times in a row
+*/
+void print_alphabet_x10(void);
+{
+	int i = 0, j;
+	while (i<10)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
