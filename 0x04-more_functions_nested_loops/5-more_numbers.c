8db7fdde7df438bf4cd6003ad3292a07c181413d
Gift Aterezi <giftaterezi@gmail.com>
2023-10-12 13:14:21 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints out numbers from 0 to 14
+ * Description: This function prints out numbers from 0 through 14
+*/
+void more_numbers(void)
+{
+	int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
