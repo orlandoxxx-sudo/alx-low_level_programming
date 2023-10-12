7ceca8cf36f15d5b18070f26a0ea4eb0b6598d38
Gift Aterezi <giftaterezi@gmail.com>
2023-10-12 16:06:00 -0700

    factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,25 +8,23 @@
 */
 unsigned long largest_prime_factor(unsigned long n)
 {
-    unsigned long i;
-
-    while (n % 2 == 0)
-    {
-        n = n / 2;
-    }
-
-    for (i = 3; i <= sqrt(n); i += 2)
-    {
-        while (n % i == 0)
-        {
-            n = n / i;
-        }
-    }
-
-    if (n > 2)
-        return n;
-    else
-        return i - 2;  /* Return the largest factor found */
+	unsigned long i;
+	
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+	}
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+	if (n > 2)
+		return (n);
+	else
+		return (i - 2);
 }
 
 /**
@@ -36,10 +34,9 @@ unsigned long largest_prime_factor(unsigned long n)
 
 int main(void)
 {
-    unsigned long num = 612852475143;
-    unsigned long largest_factor = largest_prime_factor(num);
-
-    printf("%lu\n", largest_factor);
-
-    return 0;
+	unsigned long num = 612852475143;
+	unsigned long largest_factor = largest_prime_factor(num);
+	
+	printf("%lu\n", largest_factor);
+	return (0);
 }
