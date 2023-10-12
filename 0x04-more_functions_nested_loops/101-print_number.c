162942103ef9053d84efb2c0235d390b91109230
Gift Aterezi <giftaterezi@gmail.com>
2023-10-12 16:27:03 -0700

    print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,34 +6,14 @@
  */
 void print_number(int n)
 {
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-
     if (n < 0)
     {
         _putchar('-');
         n = -n;
     }
 
-    int num_digits = 0;
-    int temp = n;
-    while (temp != 0)
-    {
-        temp /= 10;
-        num_digits++;
-    }
+    if (n / 10)
+        print_number(n / 10);
 
-    int divisor = 1;
-    for (int i = 1; i < num_digits; i++)
-        divisor *= 10;
-
-    while (divisor != 0)
-    {
-        _putchar((n / divisor) + '0');
-        n %= divisor;
-        divisor /= 10;
-    }
+    _putchar((n % 10) + '0');
 }
