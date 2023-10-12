9a8a944ad7c02909d5c187e512212b596b8f632e
Gift Aterezi <giftaterezi@gmail.com>
2023-10-12 14:07:59 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** print_line - This function prints a line on the terminal
+ * @n: number of times to print the underscore
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
