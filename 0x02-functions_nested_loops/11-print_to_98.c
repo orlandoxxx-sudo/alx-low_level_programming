3f78bded16bbff033c331d22748cad0c56eebc1a
Gift Aterezi <giftaterezi@gmail.com>
2023-10-11 22:04:47 -0700

    98 print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,21 +25,29 @@ void print_to_98(int n)
 {
     int i;
 
-    for (i = n; i != 98; i += (n <= 98) ? 1 : -1)
+    for (i = n; (n <= 98) ? (i <= 98) : (i >= 98); i += (n <= 98) ? 1 : -1)
     {
         if (i < 10 && i >= 0)
             _putchar(i + '0');
-        else
+        else if (i >= 10 && i <= 98)
         {
             _putchar(i / 10 + '0');
             _putchar(i % 10 + '0');
         }
+        else
+        {
+            _putchar('-');
+            _putchar((i / 100) + '0');
+            _putchar(((i / 10) % 10) + '0');
+            _putchar((i % 10) + '0');
+        }
 
-        _putchar(',');
-        _putchar(' ');
+        if (i != 98)
+        {
+            _putchar(',');
+            _putchar(' ');
+        }
     }
 
-    _putchar('9');
-    _putchar('8');
     _putchar('\n');
 }
