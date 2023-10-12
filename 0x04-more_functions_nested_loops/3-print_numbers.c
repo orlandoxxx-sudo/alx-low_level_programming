8e32b4908c44b46c93faa255e90d3962ebf86897
Gift Aterezi <giftaterezi@gmail.com>
2023-10-12 12:03:41 -0700

    Print Numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_numbers - prints the numbers from 0 through 9
  * Description: This function prints all numbers from 0 to 9
- * Return: Always 0 (success)
 */
 
 void print_numbers(void)
