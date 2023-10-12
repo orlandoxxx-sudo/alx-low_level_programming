157fe66bca52cbde46a553c62dfec3fcd3947da5
Gift Aterezi <giftaterezi@gmail.com>
2023-10-12 12:42:27 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,12 @@
  * print_numbers - prints the numbers from 0 through 9
  * Description: This function prints all numbers from 0 to 9
 */
-
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		{
+	{
 		_putchar(i + '0');
 	}
 	_putchar('\n');
