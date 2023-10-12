f3260f7e40231a15239dd3404dceae60b7433f7b
Gift Aterezi <giftaterezi@gmail.com>
2023-10-11 21:54:02 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
  * times_table - prints the 9 times table
  * starting with 0
 */
-void times_table(void)
+/**void times_table(void)
 {
 	int i, j, result;
 	
@@ -43,4 +43,48 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
+}
+*/
+
+void times_table(void)
+{
+    int i, j, product;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            product = i * j;
+
+            if (j == 0)
+            {
+                _putchar('0');
+                if (j < 9)
+                    _putchar(',');
+                _putchar(' ');
+            }
+            else
+            {
+                if (product < 10)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar('0' + product);
+                }
+                else
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar('0' + product / 10);
+                    _putchar('0' + product % 10);
+                }
+
+                if (j < 9)
+                    _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
 }
