408f5e43548bbc624a53138a650c46fb88f579db
Gift Aterezi <giftaterezi@gmail.com>
2023-10-12 11:42:43 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,12 +10,9 @@ void print_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i >= 0 && i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
-		i++;
 	}
-	_putchar('\n');
-	return (0);
+	_putchar'\n');
 }	
