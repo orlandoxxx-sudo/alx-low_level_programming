91ff77edb8e193c8d1dbf338340a6ffbb3bb333c
Gift Aterezi <giftaterezi@gmail.com>
2023-10-11 21:14:05 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * print_fibonacci - a function that prints the fibonacci
+ * main - entry point
+ * Description: this program prints first 50 fibonacci
+ * Return: Always 0 (Success)
+*/
+void print_fibonacci(int count) 
+{
+	int first = 1, second =2, next, i;
+
+	printf("%d, %d', first, second);
+	
+	for (i=3; i <= count; ++i)
+	{
+		next = first + second;
+		printf(", %d", next);
+		first = second;
+		second = next;
+	}
+}
+
+int main()
+{
+	int count = 50;
+
+	print_fibonacci(count);
+
+	printf("\n");
+
+	return (0);
+}
