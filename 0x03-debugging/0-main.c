816bc7ce6951faa202b5843d292f81bb1bfe7190
Gift Aterezi <giftaterezi@gmail.com>
2023-10-11 23:38:32 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,23 @@
 #include "main.h"
-
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
+ * main- Positive or negative
+ * This program checks if the number is positive negative or zero
+ * Return: Always 0 (Success)
 */
-
-int main(void)
+void positive_or_negative(int i)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
+        srand(time(0));
+        i = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (i > 0)
+        {
+                printf("%d is positive\n", i);
+        } else if (i == 0)
+        {
+                printf("%d is zero\n", i);
+        } else
+        {
+                printf("%d is negative\n", i);
+        }
         return (0);
 }
