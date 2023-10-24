8c07f6f18f0c4211f00ad593a13bd4fc8a646223
Gift Aterezi <giftaterezi@gmail.com>
2023-10-23 21:35:17 -0700

    print diag

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,16 +10,18 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
+	int sum1, sum2;
 
-	unsigned int sum, sum1;
-
-	sum = 0;
 	sum1 = 0;
+	sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	i = 0;
+	while (i < size)
 	{
-		sum += a[(size * i) + 1];
-		sum1 += a[(size * (i + 1)) - (i + 1)];
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
+
+		i++;
 	}
-	printf("%d, %d\n", sum, sum1);
+	printf("%i, %i\n", sum1, sum2);
 }
