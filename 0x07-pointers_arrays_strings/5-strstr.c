bd27e784eeab6210739154a9e99ef70925d31ed5
Gift Aterezi <giftaterezi@gmail.com>
2023-10-23 21:24:32 -0700

    chessboard

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,7 +8,7 @@
  * Return: pointer to the beginning of the located substring
  * or Null if the substring is not found
 */
-char *_strstr*char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
