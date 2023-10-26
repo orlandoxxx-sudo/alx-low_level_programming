dd6fe3b79d12f8581288cc4acb7f871522f02948
Gift Aterezi <giftaterezi@gmail.com>
2023-10-25 19:14:10 -0700

    prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**_prime_recursive - this is the main prime number recursive checker
+ * @n: this is the integer that is being checked
+ * @divider: the dividing integer checker
+ *
+ * Return: 0 if n is less than 2, 1 if divider is equal to 1
+ */
+int _prime_recursive(int n, int divider)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (divider == 1)
+	{
+		return (1);
+	}
+	else if (n % divider == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_prime_recursive(n, divider - 1));
+	}
+}
+
+/**
+ * is_prime_number - this is the prime number output
+ * @n: the integer to be used
+ * Return: the value of 0 or 1 if it is prime number
+ */
+int is_prime_number(int n)
+{
+	return (_prime_recursive(n, n - 1));
+}
