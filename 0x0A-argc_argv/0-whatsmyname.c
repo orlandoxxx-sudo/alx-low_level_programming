d13045d649cbeb02588a933a27d22cc8c3e61f67
Gift Aterezi <giftaterezi@gmail.com>
2023-10-27 18:16:30 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,6 +4,8 @@
 /**
  * main - this invokes the main library
  * Description: This programs prints it's own name
+ * @argc: counts the comand line arguments
+ * @argv: array of the set of arguments
  * Return: 0 Always Success
  */
 int main (int argc, char *argv[])
