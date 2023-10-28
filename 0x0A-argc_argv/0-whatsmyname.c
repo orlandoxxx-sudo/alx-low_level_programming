dd7145f03411bf8be2e90a5e728d6ec974e5f80a
Gift Aterezi <giftaterezi@gmail.com>
2023-10-27 18:15:06 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - this invokes the main library
+ * Description: This programs prints it's own name
+ * Return: 0 Always Success
+ */
 int main (int argc, char *argv[])
 {
 	if (argc >= 1)
