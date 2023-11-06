2b5ea8fbdbc9b1a535918218b5d1b58f752b51ea
Gift Aterezi <giftaterezi@gmail.com>
2023-11-06 07:57:02 -0800

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - this is a code that prints the file Path
+ * DESCRIPTION: This prints the name of the file
+ * Return: Always 0
+*/
 int main(void)
 {
 	printf("%s\n", __FILE__);
